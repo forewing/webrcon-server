d694eea65ee1b2ec53574869d4aff3ffc7194862
forewing <jujianai@hotmail.com>
2021-04-12 14:22:04 +0800

    add cross compile script

--- build.sh
+++ build.sh
@@ -15,5 +15,26 @@ if GIT_HASH=$(git rev-parse HEAD); then
 fi
 
 # Build server
-CGO_ENABLED=0 go build -ldflags "$LDFLAGS"
-mv webrcon-server output/
+BuildUnix(){
+    GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -ldflags "$LDFLAGS"
+    tar caf output/webrcon-server_$1_$2.tar.gz webrcon-server
+    rm webrcon-server
+}
+
+BuildWindows(){
+    GOOS=windows GOARCH=$1 CGO_ENABLED=0 go build -ldflags "$LDFLAGS"
+    zip -r output/webrcon-server_windows_$1.zip webrcon-server.exe
+    rm webrcon-server.exe
+}
+
+if [ -n "$1" ] && [ $1 = "cross" ]; then
+    BuildUnix linux amd64
+    BuildUnix linux arm64
+    BuildUnix linux arm
+    BuildUnix darwin amd64
+    BuildUnix darwin arm64
+    BuildWindows amd64
+else
+    CGO_ENABLED=0 go build -ldflags "$LDFLAGS"
+    mv webrcon-server output/
+fi
