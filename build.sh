e6b6ae221e39e83e792cec94573b70eaa465e711
forewing <jujianai@hotmail.com>
2021-04-19 15:54:23 +0800

    update cross compile script

--- build.sh
+++ build.sh
@@ -27,14 +27,86 @@ BuildWindows(){
     rm webrcon-server.exe
 }
 
-if [ -n "$1" ] && [ $1 = "cross" ]; then
-    BuildUnix linux amd64
-    BuildUnix linux arm64
-    BuildUnix linux arm
-    BuildUnix darwin amd64
-    BuildUnix darwin arm64
-    BuildWindows amd64
-else
-    CGO_ENABLED=0 go build -ldflags "$LDFLAGS"
+CMD_BASE="CGO_ENABLED=0 go build -ldflags \"${LDFLAGS}\""
+
+if [ ! -n "$1" ] || [ ! $1 = "all" ]; then
+    eval ${CMD_BASE}
     mv webrcon-server output/
+    exit 0
 fi
+
+# Cross compile
+
+compress_tar_gz(){
+    tar caf $1.tar.gz $1
+    mv $1.tar.gz output/
+    rm ${BIN_FILENAME}
+}
+
+compress_zip(){
+    zip -q -r $1.zip $1.exe
+    mv $1.zip output/
+    rm $1.exe
+}
+
+PLATFORMS=""
+PLATFORMS="$PLATFORMS aix/ppc64"
+PLATFORMS="$PLATFORMS darwin/amd64 darwin/arm64"
+PLATFORMS="$PLATFORMS dragonfly/amd64"
+PLATFORMS="$PLATFORMS freebsd/386 freebsd/amd64 freebsd/arm64"
+PLATFORMS="$PLATFORMS linux/386 linux/amd64 linux/arm64 linux/ppc64 linux/ppc64le linux/mips linux/mipsle linux/mips64le linux/riscv64 linux/s390x"
+PLATFORMS="$PLATFORMS netbsd/386 netbsd/amd64 netbsd/arm64"
+PLATFORMS="$PLATFORMS openbsd/386 openbsd/amd64 openbsd/arm64"
+PLATFORMS="$PLATFORMS plan9/386 plan9/amd64"
+PLATFORMS="$PLATFORMS solaris/amd64"
+PLATFORMS="$PLATFORMS windows/386 windows/amd64"
+
+PLATFORMS_ARM="freebsd linux netbsd openbsd plan9 windows"
+
+OUTPUT="webrcon-server"
+
+for PLATFORM in $PLATFORMS; do
+    GOOS=${PLATFORM%/*}
+    GOARCH=${PLATFORM#*/}
+    BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}"
+    BIN_FILENAME_ORINGIN="${BIN_FILENAME}"
+
+    if [ "${GOOS}" = "windows" ];
+        then BIN_FILENAME="${BIN_FILENAME}.exe"
+    fi
+
+    GO_ENV="GOOS=${GOOS} GOARCH=${GOARCH}"
+    CMD="${GO_ENV} ${CMD_BASE} -o ${BIN_FILENAME}"
+    echo "${GO_ENV}"
+    eval $CMD
+
+    if [ "${GOOS}" = "windows" ]; then
+        compress_zip ${BIN_FILENAME_ORINGIN}
+    else
+        compress_tar_gz ${BIN_FILENAME}
+    fi
+done
+
+for GOOS in $PLATFORMS_ARM; do
+    GOARCH="arm"
+    # build for each ARM version
+    for GOARM in 7 6 5; do
+        BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}${GOARM}"
+        BIN_FILENAME_ORINGIN="${BIN_FILENAME}"
+
+        if [ "${GOOS}" = "windows" ];
+            then BIN_FILENAME="${BIN_FILENAME}.exe"
+        fi
+
+        GO_ENV="GOARM=${GOARM} GOOS=${GOOS} GOARCH=${GOARCH}"
+        CMD="${GO_ENV} ${CMD_BASE} -o ${BIN_FILENAME}"
+        echo "${GO_ENV}"
+        eval "${CMD}"
+
+        if [ "${GOOS}" = "windows" ]; then
+            compress_zip ${BIN_FILENAME_ORINGIN}
+        else
+            compress_tar_gz ${BIN_FILENAME}
+        fi
+    done
+done
