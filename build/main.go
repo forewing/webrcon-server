3bc483daab4a3169d06b60c091570911b6a23424
forewing <jujianai@hotmail.com>
2021-04-28 17:42:30 +0800

    add native go build (github.com/forewing/gobuild)

---
+++ build/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/forewing/gobuild"
+)
+
+const (
+	name   = "webrcon-server"
+	module = "github.com/forewing/webrcon-server"
+)
+
+var (
+	flagAll = flag.Bool("all", false, "build for all platforms")
+
+	target = gobuild.Target{
+		Source:      ".",
+		OutputName:  name,
+		OutputPath:  "./output",
+		CleanOutput: true,
+
+		ExtraLdFlags: "-s -w",
+
+		VersionPath: module + "/version.Version",
+		HashPath:    module + "/version.Hash",
+
+		Compress:  gobuild.CompressRaw,
+		Platforms: []gobuild.Platform{{}},
+	}
+)
+
+func main() {
+	flag.Parse()
+	if *flagAll {
+		target.OutputName = fmt.Sprintf("%s-%s-%s-%s",
+			name,
+			gobuild.PlaceholderVersion,
+			gobuild.PlaceholderOS,
+			gobuild.PlaceholderArch)
+		target.Compress = gobuild.CompressAuto
+		target.Platforms = gobuild.PlatformCommon
+	}
+	target.Build()
+}
